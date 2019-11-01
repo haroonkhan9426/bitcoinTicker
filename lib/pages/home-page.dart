60846bfbb05e303bf057d2a19c2bd1f2ee00505a
Haroon khan <haroonkhan9426@gmail.com>
2019-11-01 11:37:50 +0500

    App theme customized

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bitcoin Ticker'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              child: Text('Card1'),
+            ),
+            Card(
+              child: Text('Card1'),
+            ),
+            Card(
+              child: Text('Card1'),
+            ),
+
+          ],
+        ),
+
+      ),
+    );
   }
 }
