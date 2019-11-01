03ff1020d217c389e182cc4817c470b9dfddda26
Haroon khan <haroonkhan9426@gmail.com>
2019-11-01 11:17:27 +0500

    Home Page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker/pages/home-page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
